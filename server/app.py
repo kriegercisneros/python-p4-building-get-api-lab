ffb999836ccecf32be4fcc0c9f522ca52639c7fe
JackieCisneros <krieger.jacqueline@gmail.com>
2023-04-05 09:32:27 -0600

    finished with notes

--- server/app.py
+++ server/app.py
@@ -1,78 +1,3 @@
-# #!/usr/bin/env python3
-
-# from flask import Flask, make_response, jsonify
-# from flask_migrate import Migrate
-
-# from models import db, Bakery, BakedGood
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.json.compact = False
-
-# migrate = Migrate(app, db)
-
-# db.init_app(app)
-
-# @app.route('/')
-# def index():
-#     return '<h1>Bakery GET API</h1>'
-
-# @app.route('/bakeries')
-# def bakeries():
-#     #this is where i create the models
-#     #getting all of the instances of bakeries from the DB
-#     bakeries = Bakery.query.all()
-#     #a list comprehension is used to create a serialized representation of a list of Bakery objects
-#     #bakeries is the name of the db table.  returning data to a client
-#     bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
-
-#     response = make_response(
-#         jsonify(bakeries_serialized), 200
-#     )
-
-#     response.headers['Content-Type']='application/json'
-#     return response
-
-# @app.route('/bakeries/<int:id>')
-# def bakery_by_id(id):
-#     bakery=Bakery.query.filter_by(id=id).first()
-#     bakery_serialized =bakery.to_dict()
-
-#     response=make_response(
-#         jsonify(bakery_serialized), 200
-#     )
-#     response.headers['Content-Type'] = 'application/json'
-#     return response
-
-# @app.route('/baked_goods/by_price')
-# def baked_goods_by_price():
-#     baked_goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
-#     baked_goods_by_price_serialized = [
-#         bg.to_dict() for bg in baked_goods_by_price
-#     ]
-
-#     response = make_response(
-#         jsonify(baked_goods_by_price_serialized), 200
-#     )
-#     response.header['Content-Type'] = 'application/json'
-#     return response
-
-# @app.route('/baked_goods/most_expensive')
-# def most_expensive_baked_good():
-#     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
-#     most_expensive_seralized = most_expensive.to_dict()
-
-#     response = make_response(
-#         jsonify(most_expensive_seralized), 200
-#     )
-#     response.headers['Content-Type'] = 'application/json'
-#     return response
-
-# if __name__ == '__main__':
-#     app.run(port=555, debug=True)
-
-
 #!/usr/bin/env python3
 
 from flask import Flask, make_response, jsonify
@@ -95,26 +20,27 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-
+    #this is where i create the models
+    #getting all of the instances of bakeries from the DB
     bakeries = Bakery.query.all()
+    #a list comprehension is used to create a serialized representation of a list of Bakery objects
+    #bakeries is the name of the db table.  returning data to a client
     bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
 
     response = make_response(
-        jsonify(bakeries_serialized),
-        200
+        jsonify(bakeries_serialized), 200
     )
-    response.headers['Content-Type'] = 'application/json'
+
+    response.headers['Content-Type']='application/json'
     return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
+    bakery=Bakery.query.filter_by(id=id).first()
+    bakery_serialized =bakery.to_dict()
 
-    bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
-
-    response = make_response(
-        jsonify(bakery_serialized),
-        200
+    response=make_response(
+        jsonify(bakery_serialized), 200
     )
     response.headers['Content-Type'] = 'application/json'
     return response
@@ -125,25 +51,24 @@ def baked_goods_by_price():
     baked_goods_by_price_serialized = [
         bg.to_dict() for bg in baked_goods_by_price
     ]
-    
+
     response = make_response(
-        jsonify(baked_goods_by_price_serialized),
-        200
+        jsonify(baked_goods_by_price_serialized), 200
     )
-    response.headers['Content-Type'] = 'application/json'
+    response.header['Content-Type'] = 'application/json'
     return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
-    most_expensive_serialized = most_expensive.to_dict()
+    most_expensive_seralized = most_expensive.to_dict()
 
     response = make_response(
-        jsonify(most_expensive_serialized),
-        200
+        jsonify(most_expensive_seralized), 200
     )
     response.headers['Content-Type'] = 'application/json'
     return response
 
 if __name__ == '__main__':
     app.run(port=555, debug=True)
+
--- server/seed.py
+++ server/seed.py
@@ -1,74 +1,21 @@
-# #this is the shebang line indicating the script should be run using Python3
-# #!/usr/bin/env python3
-
-# #randomint and choice are functions from the Python random module that are used to generate 
-# #random integers and make random selections from a list
-# from random import randint, choice as rc
-
-# #faker is a class from the Faker module  that is used to generate random fake data
-# from faker import Faker
-
-# #app is an instance of a Flask application ???????what does that mean??IT MEANS THAT I AM ACTUALLY IMPORTING APP FROM THE APP MODULE
-# from app import app
-
-# from models import db, Bakery, BakedGood
-
-# fake = Faker()
-
-# #this creates a context for the Flask app, whcih is necessary to interact with the db???????what does that mean??
-# with app.app_context():
-
-#     BakedGood.query.delete()
-#     Bakery.query.delete()
-    
-#     bakeries = []
-#     for i in range(20):
-#         b = Bakery(
-#             name=fake.company()
-#         )
-#         bakeries.append(b)
-    
-#     #i know this adds all the bakeries with the fake names we just created, but how and where does it add it?  
-#     #how does it know where to put it in our db.  Remember, db is just the access to all SQLAlchemy methods
-#     db.session.add_all(bakeries)
-
-#     baked_goods = []
-#     names = []
-#     for i in range(200):
-
-#         name = fake.first_name()
-#         while name in names:
-#             name = fake.first_name()
-#         names.append(name)
-
-#         bg = BakedGood(
-#             name=name,
-#             price=randint(1,10),
-#             bakery=rc(bakeries)
-#         )
-
-#         baked_goods.append(bg)
-
-#     db.session.add_all(baked_goods)
-#     db.session.commit()
-    
-#     most_expensive_baked_good = rc(baked_goods)
-#     most_expensive_baked_good.price = 100
-#     db.session.add(most_expensive_baked_good)
-#     db.session.commit()
-
-
+#this is the shebang line indicating the script should be run using Python3
 #!/usr/bin/env python3
 
+#randomint and choice are functions from the Python random module that are used to generate 
+#random integers and make random selections from a list
 from random import randint, choice as rc
 
+#faker is a class from the Faker module  that is used to generate random fake data
 from faker import Faker
 
+#app is an instance of a Flask application ???????what does that mean??IT MEANS THAT I AM ACTUALLY IMPORTING APP FROM THE APP MODULE
 from app import app
+
 from models import db, Bakery, BakedGood
 
 fake = Faker()
 
+#this creates a context for the Flask app, whcih is necessary to interact with the db???????what does that mean??
 with app.app_context():
 
     BakedGood.query.delete()
@@ -81,6 +28,8 @@ with app.app_context():
         )
         bakeries.append(b)
     
+    #i know this adds all the bakeries with the fake names we just created, but how and where does it add it?  
+    #how does it know where to put it in our db.  Remember, db is just the access to all SQLAlchemy methods
     db.session.add_all(bakeries)
 
     baked_goods = []
@@ -107,3 +56,4 @@ with app.app_context():
     most_expensive_baked_good.price = 100
     db.session.add(most_expensive_baked_good)
     db.session.commit()
+
